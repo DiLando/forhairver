df3247b9a39cbebb2b9a26000bf50f187e2ef79b
Dilando <maxime.bruxaux@gmail.com>
2017-07-18 11:58:38 +0200

    Initialisation du repository pour le site ForHairver

---
+++ config/settings.old.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestadb');
+define('_DB_USER_', 'PRESTAUSR');
+define('_DB_PASSWD_', 'f251c336ee4a4ab45173b11ece360ac71657232a9577b38a74cd20ecf342ddb8');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'W9APfOvvC5m28wgRy3FSqzJ3sPZXmMjsygbPLzBABxE5A7Suowj1arb9');
+define('_COOKIE_IV_', 'SwxTwg3o');
+define('_PS_CREATION_DATE_', '2016-07-15');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.6');
+define('_RIJNDAEL_KEY_', 'AP2TOBhcX8tuFC9ys2bOQWrtrQj4jtnj');
+define('_RIJNDAEL_IV_', '0AHQsplEB+dwJeEeX8onPQ==');
